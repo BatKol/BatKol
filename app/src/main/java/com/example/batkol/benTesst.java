ca85fb1acc4be6fa792a4dd02d88560fdf362a49
Benitk <beny9996@gmail.com>
2021-03-16 01:03:02 +0200

    add recordCard class and recordlist adapter

--- app/src/main/java/com/example/batkol/benTesst.java
+++ app/src/main/java/com/example/batkol/benTesst.java
@@ -7,29 +7,76 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+import models.RecordCard;
+import utils.RecordList_adapter;
 
 public class benTesst extends AppCompatActivity
 {
 
+    private RecyclerView recyclerView;
+    private ArrayList<RecordCard> cards;
+    private RecordList_adapter cardsAdapter;
+    private ProgressBar progressb;
+
+    public   static boolean flag;
+    public static boolean b;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ben_tesst);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        cards = new ArrayList<RecordCard>();
+
+        progressb = (ProgressBar)findViewById(R.id.progressBar);
+        progressb.setVisibility(View.GONE);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        initRecyclerAdapter();
+        addNewCard("Liad", "now");
+        addNewCard("Ben", "1 day ago");
+        addNewCard("Amichai", "3 min ago");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+    }
+
+    // create new card from given snapshot of seller products
+    private void addNewCard(String creator, String date){
+
+        RecordCard card = new RecordCard();
+
+        card.setCreatorName(creator);
+
+        card.setPublishDate(date);
+
+        cards.add(card);
+
+        cardsAdapter.notifyDataSetChanged();
+    }
+
+
+    // print the cards arrays on the current activity recyclerView
+    private void initRecyclerAdapter() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cardsAdapter = new RecordList_adapter(this,cards);
+        recyclerView.setAdapter(cardsAdapter);
+
     }
 }
