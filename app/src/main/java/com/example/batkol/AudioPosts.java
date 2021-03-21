86830b7a76ba4f91dd0d636f8a4dd8bda752006a
amichaihadad <amichaiaaa@gmail.com>
2021-03-21 19:10:25 +0200

    fix some things

--- app/src/main/java/com/example/batkol/AudioPosts.java
+++ app/src/main/java/com/example/batkol/AudioPosts.java
@@ -4,11 +4,11 @@ import java.util.Date;
 
 public class AudioPosts {
     final String creator, description, url;
-    final Date date;
+    final Date date1;
 
     public AudioPosts(String creator, Date date, String description, String url) {
         this.creator = creator;
-        this.date = date;
+        this.date1 = date;
         this.description = description;
         this.url = url;
     }
